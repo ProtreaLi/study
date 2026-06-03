8388518bf968f4897c9dbe7fb8fb78b13bc0bd9a
ProtreaLi <li18835531849@163.com>
2026-06-03 17:13:04 +0800

    作业1

--- class/lesson2.1-数据类型基础/Homework.sol
+++ class/lesson2.1-数据类型基础/Homework.sol
@@ -7,26 +7,41 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 //2.尝试使用不同大小的uint类型存储相同的数据，并观察其Gas消耗差异。
 //3.编写代码测试Solidity0.8版本前后整数溢出的行为
 contract Task {
+ 
    using Strings for uint256;
    uint256 public a = 666;
    string public b = "888";
 
-    //uint256 转 string
+    //作业1:值类型基本使用
+    function homework1_basic_use_value() public view returns (uint256) {
+        uint256 result = a + 100;  // 加法操作
+        return result;
+    }
+    //作业1:引用类型基本使用
+    function homework1_basic_use_ref() public view returns (string memory){
+        // string拼接操作
+        string memory newString = string(abi.encodePacked(b, "hello"));
+        return newString;
+    }
+
+    //作业1:uint256 转 string
     function a2b() public {
+        a = 666;
         b = a.toString(); 
     }
 
-    //string 转 uint256
+    //作业1:string 转 uint256
     function b2a() public {
+        b = "888";
         a = stringToUint(b);
     }
 
-    // 辅助函数：string 转 uint256
+    //作业1:辅助函数：string 转 uint256
     function stringToUint(string memory s)   internal  pure returns (uint256) {
-        bytes memory b = bytes(s);
+        bytes memory k = bytes(s);
         uint256 result = 0;
-        for(uint i = 0; i < b.length; i++) {
-            uint8 char = uint8(b[i]);
+        for(uint i = 0; i < k.length; i++) {
+            uint8 char = uint8(k[i]);
             require(char >= 48 && char <= 57, "Invalid character");
             result = result * 10 + (char - 48);
         }
